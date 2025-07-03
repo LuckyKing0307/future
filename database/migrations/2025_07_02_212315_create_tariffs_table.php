a373b2594e331580e124a92d607d1020761d6613
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-03 21:17:37 +0500

    first commit

--- database/migrations/2025_07_02_212315_create_tariffs_table.php
+++ database/migrations/2025_07_02_212315_create_tariffs_table.php
@@ -22,9 +22,7 @@ return new class extends Migration
         });
 
         Schema::table('users', function (Blueprint $table) {
-            $table->id();
             $table->string('tariff_id');
-            $table->timestamps();
         });
     }
 
