45c33cf6e07f90b38c75af8674121b411dc90193
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-03 21:20:30 +0500

    first commit

--- database/migrations/2024_10_05_125411_add_photo_tasks.php
+++ database/migrations/2024_10_05_125411_add_photo_tasks.php
@@ -22,7 +22,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('photo');
         });
     }
 };
