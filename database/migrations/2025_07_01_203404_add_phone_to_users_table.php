3be26bc723fbfc58aca92f9b58e63377c5cded0b
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-04 19:03:06 +0500

    first commit

--- database/migrations/2025_07_01_203404_add_phone_to_users_table.php
+++ database/migrations/2025_07_01_203404_add_phone_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->unique()->after('id');
+            $table->string('phone')->unique()->after('id')->nullable();
             $table->string('email')->nullable()->change();   // e-mail больше не обязателен
         });
     }
