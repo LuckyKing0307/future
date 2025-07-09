271e8a72eac10a1b6cc89e2a60ffcc6e233c726d
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-09 17:58:55 +0500

    first commit

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -2,7 +2,7 @@
     <div class="text-black px-2 mt-2 w-25">
         Фотографии Задачи
         @foreach($photos as $photo)
-            <img src="{{$photo}}" alt="">
+            <img src="{{$photo}}" alt="" style="display: block; min-width: 100px; max-width: 300px;">
         @endforeach
     </div>
 </div>
